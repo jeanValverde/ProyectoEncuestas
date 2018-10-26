c38ff951faaa46270a30706efff8a3fd8e082329
jeanValverde <44486964+jeanValverde@users.noreply.github.com>
2018-10-26 00:38:48 -0300

    Add files via upload

---
+++ application/controllers/Encuestas.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Encuestas
+ *
+ * @author jean
+ */
+class Encuestas extends CI_Controller {
+    
+    public function index(){
+        //carga la vista de los diseños 
+    }
+    
+    
+    public function agregarEncuesta(){
+        
+    }
+    
+   
+    public function agregarRespuesta(){
+        
+    }
+   
+}
